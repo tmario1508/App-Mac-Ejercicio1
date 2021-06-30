aeb5e3d2b1035e48eb60e68f922ee229434487b3
Mario <tmario1508@gmail.com>
2021-06-29 20:03:01 -0700

    Creación inicial de ventanas

---
+++ aapControlesBasicos/CustomViewController.h
@@ -0,0 +1,16 @@
+//
+//  CustomViewController.h
+//  aapControlesBasicos
+//
+//  Created by MTWDM301-2021 on 29/06/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomViewController : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ aapControlesBasicos/CustomViewController.m
@@ -0,0 +1,21 @@
+//
+//  CustomViewController.m
+//  aapControlesBasicos
+//
+//  Created by MTWDM301-2021 on 29/06/21.
+//
+
+#import "CustomViewController.h"
+
+@interface CustomViewController ()
+
+@end
+
+@implementation CustomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
